ce0ea77b25ff021135f0316d0bda3a8ddd56191b
Cristian David Home <cristian_david033@hotmail.com>
2020-12-22 15:13:22 -0500

    🚧 Crear middleware ToLowerCase. - Middleware para transformar campos de texto de una petición a minúsculas.

---
+++ app/Http/Middleware/ToLowerCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ToLowerCase
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $except = ["password"];
+
+        foreach ($request->all() as $key => $value) {
+            if (is_string($value)) {
+                $request[$key] = in_array($key, $except, true) ? $value : strtolower($value);
+            }
+        }
+
+        return $next($request);
+    }
+}
