44e935f846dd448450ca2c49ea77336209968995
Cristian David Home <cristian_david033@hotmail.com>
2020-12-27 19:25:35 -0500

    🚧 Crear middleware EnsureEmailIsVerified personalizado, crear traducción para mensaje de error y configurar el idioma de la app como español.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,7 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         'to.lower' => \App\Http\Middleware\ToLowerCase::class,
     ];
 }
