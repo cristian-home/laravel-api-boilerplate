5a403a163ae526cfd9bd85aced0b08751b5fd23f
Cristian David Home <cristian_david033@hotmail.com>
2021-01-23 17:55:31 -0500

    ✅ Pruebas unitarias para funcionalidad de autenticación y crud de usuarios.

---
+++ tests/Feature/Http/Auth/RegisterControllerTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature\Http\Auth;
+
+use App\Models\User;
+use App\Notifications\VerifyEmailNotification;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+
+class RegisterControllerTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testUserCanRegisterViaApiRest()
+    {
+        Notification::fake();
+
+        $email = $this->faker->safeEmail;
+        $password = $this->faker->password;
+
+        $response = $this->postJson(
+            route('auth.register'),
+            [
+                'email' => $email,
+                'password' => $password,
+                'password_confirmation' => $password
+            ]
+        );
+
+        $user = User::where('email', $email)->first();
+
+        Notification::assertSentTo(
+            [$user],
+            VerifyEmailNotification::class
+        );
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure(['message']);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testVerifyEmailNotificationIsSentToUser()
+    {
+        Notification::fake();
+
+        $email = $this->faker->safeEmail;
+        $password = $this->faker->password;
+
+        $this->postJson(
+            route('auth.register'),
+            [
+                'email' => $email,
+                'password' => $password,
+                'password_confirmation' => $password
+            ]
+        );
+
+        $user = User::where('email', $email)->first();
+
+        Notification::assertSentTo(
+            [$user],
+            VerifyEmailNotification::class
+        );
+    }
+}
--- tests/Feature/Http/Controllers/UsersControllerTest.php
+++ tests/Feature/Http/Controllers/UsersControllerTest.php
@@ -2,16 +2,16 @@
 
 namespace Tests\Feature\Http\Controllers;
 
-use App\Events\NewUser;
+use Tests\TestCase;
 use App\Models\User;
-use App\Notification\NewUserNotification;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Event;
+use App\Events\NewUser;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Event;
+use App\Notifications\NewUserNotification;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Notification;
 use JMac\Testing\Traits\AdditionalAssertions;
-use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 /**
  * @see \App\Http\Controllers\UsersController
@@ -25,12 +25,26 @@ class UsersControllerTest extends TestCase
      */
     public function index_behaves_as_expected()
     {
-        $users = User::factory()->count(3)->create();
+        $this->getJson(route('users.index'))->assertUnauthorized();
+
+        $this->actingAs(User::factory()->create(), 'api');
+
+        User::factory()->count(3)->create();
 
         $response = $this->getJson(route('users.index'));
 
         $response->assertOk();
-        $response->assertJsonStructure([]);
+        $response->assertJsonStructure(
+            [
+                "data" => [
+                    [
+                        "id",
+                        "email",
+                        "email_verified_at",
+                    ]
+                ]
+            ]
+        );
     }
 
 
@@ -51,11 +65,10 @@ class UsersControllerTest extends TestCase
      */
     public function store_saves()
     {
+        $this->actingAs(User::factory()->create(), 'api');
+
         $email = $this->faker->safeEmail;
         $password = $this->faker->password;
-
-        Notification::fake();
-        Event::fake();
 
         $response = $this->postJson(route('users.store'), [
             'email' => $email,
@@ -73,13 +86,48 @@ class UsersControllerTest extends TestCase
         $user = $users->first();
 
         $this->assertTrue(Hash::check($password, $user->password));
+    }
 
-        Notification::assertSentTo($user, NewUserNotification::class, function ($notification) use ($user) {
-            return $notification->user->is($user);
-        });
+    /**
+     * @test
+     */
+    public function store_fire_event_new_user()
+    {
+        $this->actingAs(User::factory()->create(), 'api');
+
+        $email = $this->faker->safeEmail;
+        $password = $this->faker->password;
+
+        Event::fake();
+        Notification::fake();
+
+        $response = $this->postJson(route('users.store'), [
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        $user = User::where('email', $email)->first();
 
         Event::assertDispatched(NewUser::class, function ($event) use ($user) {
             return $event->user->is($user);
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function new_user_event_send_new_user_notification()
+    {
+        Notification::fake();
+
+        $user = User::factory()->create();
+
+        $event = new NewUser($user);
+
+        event($event);
+
+        Notification::assertSentTo($user, NewUserNotification::class, function ($notification) use ($user) {
+            return $notification->user->is($user);
         });
     }
 
@@ -89,12 +137,22 @@ class UsersControllerTest extends TestCase
      */
     public function show_behaves_as_expected()
     {
+        $this->actingAs(User::factory()->create(), 'api');
+
         $user = User::factory()->create();
 
         $response = $this->get(route('users.show', $user));
 
         $response->assertOk();
-        $response->assertJsonStructure([]);
+        $response->assertJsonStructure(
+            [
+                "data" => [
+                    "id",
+                    "email",
+                    "email_verified_at"
+                ]
+            ]
+        );
     }
 
 
@@ -115,6 +173,8 @@ class UsersControllerTest extends TestCase
      */
     public function update_behaves_as_expected()
     {
+        $this->actingAs(User::factory()->create(), 'api');
+
         $user = User::factory()->create();
         $email = $this->faker->safeEmail;
 
@@ -133,6 +193,8 @@ class UsersControllerTest extends TestCase
      */
     public function destroy_deletes_and_responds_with()
     {
+        $this->actingAs(User::factory()->create(), 'api');
+
         $user = User::factory()->create();
 
         $response = $this->delete(route('users.destroy', $user));
