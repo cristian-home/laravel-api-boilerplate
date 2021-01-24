a3dae88215d4e47ef6b966df2050396e38e1e12c
Cristian David Home <cristian_david033@hotmail.com>
2021-01-23 19:45:00 -0500

    🚧 Instalación y configuración de eslint + prettier. - ESLint linter para javascript. - Prettier: formateador de código.

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: ["eslint:recommended", "plugin:vue/essential"],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module"
+    },
+    plugins: ["vue"],
+    rules: {
+        // allow async-await
+        "generator-star-spacing": "off",
+        // allow debugger during development
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "vue/singleline-html-element-content-newline": "off",
+        "vue/multiline-html-element-content-newline": "off",
+        "vue/max-attributes-per-line": [
+            "error",
+            {
+                singleline: 6
+            }
+        ],
+        "vue/html-self-closing": [
+            "error",
+            {
+                html: {
+                    void: "any",
+                    normal: "always",
+                    component: "any"
+                },
+                svg: "always",
+                math: "always"
+            }
+        ],
+        "no-console": "off",
+        "vue/html-indent": ["error", 4]
+    }
+};
