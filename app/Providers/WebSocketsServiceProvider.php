64601d62e9dc6d8dbed5cfa2b172161e1e86e16b
Cristian David Home <cristian_david033@hotmail.com>
2021-03-01 05:30:41 -0500

    🚧 Ajuste de providers Telescope, Horizon, Websockets. - Ajustar Gates para acceso de usuarios especificos. - Especificar middlewares de acceso (auth, 2fa).

---
+++ app/Providers/WebSocketsServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class WebSocketsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Gate para laravel websocket
+        Gate::define('viewWebSocketsDashboard', function ($user = null) {
+            return in_array($user->email, ['chome@cpe.gov.co']);
+        });
+    }
+}
