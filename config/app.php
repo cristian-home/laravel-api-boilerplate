5caa7c1c1bef0ec43f7d99f3f927fdd78209acd8
Cristian David Home <cristian_david033@hotmail.com>
2021-03-02 16:33:58 -0500

    🚧 Crear trait y service provider para paginables. -  PaginationServiceProvider: permite establecer número de items por página con parametros de URL con el metodo paginateAuto() ej. User::paginateAuto(). - Trait Paginatable: permite lo mismo que  PaginationServiceProvider para usar como trait y haciendo uso de paginate().

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
          * Custom Service Providers...
          */
         App\Providers\WebSocketsServiceProvider::class,
+        App\Providers\PaginationServiceProvider::class,
     ],
 
     /*
