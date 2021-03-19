ccc0b0f0458ea28fb4d0aba38fab3859f58c25ff
Cristian David Home <cristian_david033@hotmail.com>
2021-03-19 16:48:15 -0500

    🚧 Construir frontend básico en Vue.

---
+++ resources/js/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="app">
+        <div class="locale-changer">
+            <select v-model="$i18n.locale">
+                <option
+                    v-for="(lang, i) in langs"
+                    :key="`Lang${i}`"
+                    :value="lang"
+                >
+                    {{ lang }}
+                </option>
+            </select>
+        </div>
+        <div id="nav">
+            <router-link to="/">{{ $t('Home') }}</router-link>
+            |
+            <router-link to="/about">{{ $t('About') }}</router-link>
+        </div>
+        <router-view />
+    </div>
+</template>
+<script>
+export default {
+    data: () => ({
+        langs: ['es', 'en']
+    })
+};
+</script>
+
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+.locale-changer {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+}
+
+#nav {
+    padding: 30px;
+
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+
+        &.router-link-exact-active {
+            color: #42b983;
+        }
+    }
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,27 @@
-require('./bootstrap');
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import VueI18n from 'vue-i18n';
+
+Vue.config.productionTip = false;
+
+import messages from '@kirschbaum-development/laravel-translations-loader!@kirschbaum-development/laravel-translations-loader';
+
+console.log(messages);
+
+Vue.use(VueI18n);
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+    locale: 'es', // set locale
+    messages, // set locale messages
+  })
+
+new Vue({
+    router,
+    store,
+    i18n,
+    render: h => h(App)
+}).$mount('#app');
